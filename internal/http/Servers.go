e7663c00106d2b998037c098f718fc8c7bfeaf60
Nevrd <aromur0404@gmail.com>
2026-04-29 20:24:18 +0300

    fix delete path

--- internal/http/Servers.go
+++ internal/http/Servers.go
@@ -23,7 +23,7 @@ func (s *HTTPServer) StartServer() error {
 	s.router.Path("/posts").Methods(http.MethodPost).HandlerFunc(s.httpHandlers.CreatePost) // created Post
 	s.router.Path("/posts").Methods(http.MethodGet).HandlerFunc(s.httpHandlers.GetPosts)
 	s.router.Path("/posts/{title}").Methods(http.MethodGet).HandlerFunc(s.httpHandlers.GetPost)
-	s.router.Path("/post/{title}").Methods(http.MethodDelete).HandlerFunc(s.httpHandlers.DeletePost)
+	s.router.Path("/posts/{title}").Methods(http.MethodDelete).HandlerFunc(s.httpHandlers.DeletePost)
 
 	err := http.ListenAndServe(":8080", s.router)
 	if errors.Is(err, http.ErrServerClosed) {
